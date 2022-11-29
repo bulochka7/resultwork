b9806e53586519e9f157cdeb23b86a23cfa0fcfc
bulochka7 <114214964+bulochka7@users.noreply.github.com>
2022-11-29 18:32:02 +0300

    Add files via upload

---
+++ dzsupersrochno/exam/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] Array()
+{
+    string[] array=new string[5];
+    for(int i=0;i<array.Length;i++)
+        {
+           array[i]=Convert.ToString(Console.ReadLine());
+        }
+    return array;
+}
+
+void FindArray(string[] array)
+{
+    for(int i=0;i<array.Length;i++)
+        if(array[i].Length<=3)
+        {
+            Console.Write(array[i]+" ");
+        }
+}
+Console.WriteLine("Введите 5 значений через enter ");
+string[] array=Array();
+FindArray(array);
